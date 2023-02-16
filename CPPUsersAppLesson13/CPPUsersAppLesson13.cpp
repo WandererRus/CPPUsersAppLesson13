7df9a9162861795b9ebb2bb6f1458d33db3b57dd
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-02-16 20:04:29 +0300

    13

--- CPPUsersAppLesson13/CPPUsersAppLesson13.cpp
+++ CPPUsersAppLesson13/CPPUsersAppLesson13.cpp
@@ -2,10 +2,88 @@
 //
 
 #include <iostream>
-
+#include <string>
 using namespace std;
+string russianAlphabet[33]{ "щ","ж","х","ц","ч","ш","ю","я","а","б","в","г","д","е","ё","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","ъ","ы","ь","э" };
+string russianTranslateAlphabet[33]{ "shch","zh","kh","tc","ch","sh","iu","ia","a","b","v","g","d","e","e","z","i","i","k","l","m","n","o","p","r","s","t","u","f","\"","y","'","e" };
+
+string ToRussian(string text)
+{
+    int length = text.length();
+    string translate = "";
+    string fullchar = "";
+    bool find = false;
+    for (int i = 0; i < length; i++)
+    {
+        find = false;
+        for (int j = 0; j < 33; j++)
+        {
+            if (russianTranslateAlphabet[j].length() > 1)
+            { 
+                fullchar = "";
+                if (i + russianTranslateAlphabet[j].length()-1 < length)
+                { 
+                    for (int k = 0; k < russianTranslateAlphabet[j].length(); k++)
+                    {
+                        fullchar.push_back((char)tolower(text[i+k]));
+                    }
+                    if (russianTranslateAlphabet[j] == fullchar)
+                    {
+                        translate += russianAlphabet[j];
+                        find = true;
+                        i += russianTranslateAlphabet[j].length() - 1;
+                        break;
+                    }                                                         
+                }
+            }
+            else
+            {
+                if (russianTranslateAlphabet[j].find((char)tolower(text[i])) == 0)
+                {
+                    translate += russianAlphabet[j];
+                    find = true;
+                    break;
+                }
+            }
+            
+        }
+        if (!find)
+        {
+            translate.push_back(text[i]);
+        }
+    }
+    return translate;
+}
+string ToEnglish(string text)
+{
+    int length = text.length();
+    string translate = "";
+    bool find = false;
+    for (int i = 0; i < length; i++)
+    {
+        find = false;
+        for (int j = 0; j < 33; j++)
+        {
+            if (russianAlphabet[j].find((char)tolower(text[i])) == 0)
+            {
+                translate += russianTranslateAlphabet[j];
+                find = true;
+                break;
+            }            
+        }
+        if (!find)
+        {
+            translate.push_back(text[i]);
+        }
+    }
+
+    return translate;
+}
+
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+    
     ////Task1
     //FILE* fileWords;
     //FILE* fileText;
@@ -51,10 +129,37 @@ int main()
     //    fputs(str.c_str(), fileResult);
     //}
 
-   /* Задание 2. Написать программу транслитерации с русского
-        на английский и наоборот.Данные для транслитерации
+   /* Задание 2. Данные для транслитерации
         берутся из файла и записываются в другой файл.Направление перевода определяется через меню пользователя.*/
-
+    
+    FILE* fileorigin;
+    string s = "";
+    fopen_s(&fileorigin, "D:\\task13_2.txt", "r");
+    
+    while (!feof(fileorigin))
+    {
+         s += fgets(new char[128],127, fileorigin);
+    }
+    fclose(fileorigin);
+    
+    cout << s << "\n";
+    cout << "В какой язык переводить? \n На русский - r. На английский - e." << "\n";
+    char direction = ' ';
+    cin >> direction;
+    string snew = "";
+    if (direction = 'e')
+    {
+        snew = ToRussian(s);
+    }
+    if (direction = 'e')
+    {
+        snew = ToEnglish(s);
+    }
+    fopen_s(&fileorigin, "D:\\task13_2_result.txt", "w");
+    fputs(snew.c_str(), fileorigin);
+    fclose(fileorigin);
+    cout << snew << "\n";
+    
     return 0;
 }
 
